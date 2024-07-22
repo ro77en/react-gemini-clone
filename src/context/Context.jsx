a02b788752c8033dd3a102a65370103b1c375a2c
ro77en <lucas.mdantas@icloud.com>
2024-07-22 15:25:15 -0300

    gemini api integration

---
+++ src/context/Context.jsx
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+
+    const onSent = async (prompt) => {
+        await run(prompt);
+    }
+
+    onSent("What is React JS?");
+
+    const contextValue = {
+
+    }
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+
+}
+
+export default ContextProvider;
